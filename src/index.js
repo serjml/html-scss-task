62e16ad27f4f357f7e3f07a90a6a319d78523024
Сергей Ляхов <serjml@Mac-Serjml.local>
2023-05-21 16:45:41 +0300

    prepare for deploy

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import addSelectClickHandler from './scripts/select';
-import addRangeInputHandler from './scripts/range';
-import addHamburgerClickHandler from './scripts/hamburger';
-
-window.addEventListener('DOMContentLoaded', () => {
-  addSelectClickHandler();
-  addRangeInputHandler();
-  addHamburgerClickHandler();
-});
